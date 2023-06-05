b6544e9e4628ad530f6870282a26a610e907efaa
Matthew Hughes <matthewhughes934@gmail.com>
2023-06-05 18:13:45 +0100

    Move `generate` out of `node.go`
    It didn't make too much sense to have this in functionality-specific
    file

---
+++ go-cmark/cmark.go
@@ -0,0 +1,4 @@
+// Go bindings for cmark library
+
+//go:generate ../scripts/copy-lib ../cmark
+package cmark
--- go-cmark/node.go
+++ go-cmark/node.go
@@ -1,6 +1,3 @@
-// Go bindings for cmark library
-
-//go:generate ../scripts/copy-lib ../cmark
 package cmark
 
 /*
