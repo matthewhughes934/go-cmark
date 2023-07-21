2a20361658b2d02bc9525d43e4af86c8501fa704
Matthew Hughes <matthewhughes934@gmail.com>
2023-07-21 20:11:06 +0100

    cmark: move examples to their own file
    Again, just copying changes over from gfm:
    c210bed5b3f93f079cecd6207e5e6edac55b66e0

---
+++ pkg/cmark/cmark_test.go
@@ -0,0 +1,33 @@
+package cmark
+
+import (
+	"fmt"
+)
+
+func Example() {
+	document := "# My great document\n\nWhat a great read!\n"
+	root := ParseDocument(document, NewParserOpts())
+
+	heading := root.FirstChild()
+	headingContent := heading.FirstChild()
+
+	paragraph := heading.Next()
+	paragraphContent := paragraph.FirstChild()
+
+	fmt.Println(root.GetTypeString())
+	fmt.Println(heading.GetTypeString())
+	fmt.Println(headingContent.GetType() == NodeTypeText)
+	fmt.Println(*headingContent.GetLiteral())
+	fmt.Println(paragraph.GetTypeString())
+	fmt.Println(paragraphContent.GetType() == NodeTypeText)
+	fmt.Println(*paragraphContent.GetLiteral())
+
+	// Output:
+	// document
+	// heading
+	// true
+	// My great document
+	// paragraph
+	// true
+	// What a great read!
+}
